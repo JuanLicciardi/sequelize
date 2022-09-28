2e9f85acb3e8916c6d7110d32158b50a9c23d00e
JuanLicciardi <juanelicciardi@gmail.com>
2022-09-28 17:44:19 -0300

    Configurando Sequelize

---
+++ src/controllers/moviesController.js
@@ -0,0 +1,21 @@
+const db = require('../database/models')
+
+module.exports={
+
+    list:(req,res) =>{
+
+    },
+
+    new:(req,res) =>{
+
+    },
+
+    recommended:(req,res) =>{
+
+    },
+
+    detail:(req,res) =>{
+
+    }
+
+}
