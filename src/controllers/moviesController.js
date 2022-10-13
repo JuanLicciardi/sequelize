885101f8e9f14cecce1515c327e45eacd6590b9c
JuanLicciardi <juanelicciardi@gmail.com>
2022-10-13 00:03:01 -0300

    Finalizando Editar Peliculas

--- src/controllers/moviesController.js
+++ src/controllers/moviesController.js
@@ -1,4 +1,6 @@
 const db = require('../database/models')
+const sequelize = db.sequelize;
+const moment = require('moment');
 
 module.exports={
 
@@ -47,16 +49,62 @@ module.exports={
 
     //Aqui debemos modificar y completar lo necesario para trabajar con el CRUD
     add: function (req, res) {
-        // TODO   
+        // TODO 
+        db.Genre.findAll({
+            order : ['name']
+        })
+            .then(genres => res.render('moviesAdd',{
+                genres
+            }))
+            .catch(error => console.log(error))  
     },
+
     create: function (req, res) {
-        // TODO
+        const {title,release_date, rating,awards,genre_id,length} = req.body
+        db.Movie.create({
+            ...req.body,
+            title:title.trim(),
+        })
+        .then(movie => {
+            console.log(movie)
+            return res.redirect('/movies/detail/' + movie.id)
+        })
+        .catch(error => console.log(error))
     },
+
     edit: function(req, res) {
-        // TODO
+        let genres = db.Genre.findAll({
+            order : ['name']
+        });
+
+        let movie = db.Movie.findByPk(req.params.id);
+
+        Promise.all([genres,movie])
+        .then(([genres,movie]) => {
+            res.render('moviesEdit',{
+                genres,
+                Movie:movie,
+                moment : moment
+            });
+        })
+        .catch((error) => console.log(error))
+        
     },
     update: function (req,res) {
-        // TODO
+        db.Movie.update(
+            {
+                ...req.body,
+                title : req.body.title.trim()
+            },
+            {
+                where : {id:req.params.id}
+            }
+        )
+        .then (response => {
+            return res.redirect('/movies/detail/' + req.params.id)
+        })
+        .catch(error => console.log(error))
+        
     },
     delete: function (req, res) {
         // TODO
